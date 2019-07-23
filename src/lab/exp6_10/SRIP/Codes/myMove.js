53c1cda0c03f9d6f76e43d331705c7172e58ce9e
vivi chan <vetha2naidu@gmail.com>
2019-07-23 20:10:32 +0530

    JS files and calling functions set accordingly

---
+++ src/lab/exp6_10/SRIP/Codes/myMove.js
@@ -0,0 +1,16 @@
+<script>
+    function myMove(){
+    var elem=document.getElementById("animate");
+    var pos=0;
+    var id=setInterval(frame,7);
+    function frame(){
+      if(pos==100){
+        clearInterval(id);
+    } else{
+        pos++;
+    elem.style.left=pos+"px";
+    elem.style.right=pos+"px";
+  }
+  }
+}
+  </script>
