f8fb58d614c14b6937f9edc8c1ee5353a6dabe2d
vivi chan <vetha2naidu@gmail.com>
2019-07-25 10:42:45 +0530

    another animation

---
+++ src/lab/exp6_10/SRIP/Codes/fun6.js
@@ -0,0 +1,4 @@
+function myFunction5() {
+    document.getElementById('').innerHTML = "Click on the sample solution to draw 1mL of the solution with a syringe";
+    document.getElementById('ir-cell').classList.toggle("hide");
+}
